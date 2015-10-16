64084d7d88e5b11d5068f695191ac5865482e4da
Chris Piro <cpiro@cpiro.com>
2015-10-16 17:33:44 -0400

    todo, setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-from future import standard_library
-standard_library.install_aliases()
 from setuptools import setup, find_packages
 
 setup(
@@ -26,6 +20,6 @@ setup(
     author_email = "cpiro@cpiro.com",
     description = "adds ZFS zpool and vdev information, filtering, and sorting, to lsblk(8)",
     license = "GPL",
-    keywords = "lsblk zfs console xxx",
-    url = "xxx",
+    keywords = "lsblk zfs zpool console",
+    url = "https://github.com/cpiro/lsblkpro",
 )
