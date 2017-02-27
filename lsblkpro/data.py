071a27fcab4b7fafa6003e6fa99abb3dfe323608
Chris Piro <cpiro@cpiro.com>
2017-02-26 23:20:32 -0500

    add zd to name_parts_for splitter

--- lsblkpro/data.py
+++ lsblkpro/data.py
@@ -61,7 +61,7 @@ class Device(Entity):
                 return p
 
         # https://www.kernel.org/doc/Documentation/devices.txt
-        tup = tuple(to_int_maybe(part) for part in re.findall(r'(?:^(?:ram|fd|hd|loop|sd|n?st|md|scd|n?tpqic|xd|sonycd|gscd|optcd|sjcd|c?double|hitcd|sg|mfm|hd|mcd|cdu535|sbpcd|qft|nqft|zqft|nzqft|rawqft|nrawqft|ad|aztcd|cm205cd|r?rom|r?flash|cm206cd|slram|n?ht|z2ram|nb|ft|pd|pcd|pf|r?pda|sch|mtdr?|ppdd|nft|dasd|n?pt|inft|pg|ubd|jsfd|nnpfs|ub|xvd|n?osst|rfd|ssfdc|blockrom|osd)|^[a-z]-?|[a-z]+|\d+)', name))
+        tup = tuple(to_int_maybe(part) for part in re.findall(r'(?:^(?:zd|ram|fd|hd|loop|sd|n?st|md|scd|n?tpqic|xd|sonycd|gscd|optcd|sjcd|c?double|hitcd|sg|mfm|hd|mcd|cdu535|sbpcd|qft|nqft|zqft|nzqft|rawqft|nrawqft|ad|aztcd|cm205cd|r?rom|r?flash|cm206cd|slram|n?ht|z2ram|nb|ft|pd|pcd|pf|r?pda|sch|mtdr?|ppdd|nft|dasd|n?pt|inft|pg|ubd|jsfd|nnpfs|ub|xvd|n?osst|rfd|ssfdc|blockrom|osd)|^[a-z]-?|[a-z]+|\d+)', name))
         assert ''.join(str(part) for part in tup) == name
         return tup
 
